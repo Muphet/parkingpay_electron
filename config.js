994f69a8c175c70c730f45c8bc18a6815d9b34f9
wenfp <wenfp@palmtrends.com>
2018-02-01 15:12:12 +0800

    修正冲突

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use strict';
 
 var nconf = require('nconf').file({file: './config.json'});
@@ -21,27 +21,3 @@ module.exports = {
   saveSetting,
   readSetting
 }
-=======
-'use strict';
-
-var nconf = require('nconf').file({file: './config.json'});
-
-
-function getHomeUser(){
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE': 'HOME']
-}
-function saveSetting(settingkey, settingvalue){
-  nconf.set(settingkey, settingvalue);
-  nconf.save();
-}
-
-function readSetting(settingkey){
-  nconf.load();
-  return nconf.get(settingkey)
-}
-
-module.exports = {
-  saveSetting,
-  readSetting
-}
->>>>>>> fd120f51cf3f114e2a16e4d609fba197b95cc40a
